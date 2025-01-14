646222ecb23c625dc1c9c5b0a8ee6176ec63a800
Aswinshaji1 <92560292+Aswinshaji1@users.noreply.github.com>
2025-01-14 11:46:24 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,6 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || 'add your sesson id',
+    SESSION_ID: process.env.SESSION_ID || '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=73oCVBjI#KxdRqRiX1kauzlwts78bZ9eEO2VTTcr9sKdaikI5BEE',
     MONGODB: process.env.MONGODB || 'mongodb+srv://udavin56:1234@cluster0.urhma.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
 };
